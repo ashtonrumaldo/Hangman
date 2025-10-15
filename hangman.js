314ab9921864d50728976801efb99d5d79ab4f51
Ashton Rumaldo <ashtonrumaldo@gmail.com>
2025-10-15 10:43:28 -0400

    updated js

---
+++ hangman.js
@@ -0,0 +1,5 @@
+var POSSIBLE_WORDS = ["obdurate", "juggernaut", "catalyst", "quixotic", "zephyr", "xenophobia", "serendipity", "ephemeral", "labyrinth", "mellifluous"];
+
+var randomIndex = parseInt(Math.random() * POSSIBLE_WORDS.length);
+var word = POSSIBLE_WORDS[randomIndex];
+var answerArray = [];
